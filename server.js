257efa81d52466fbb33958aef08131ab0c9ba636
Pavel Darii <paveldarii@yahoo.com>
2021-01-05 19:32:43 -0500

    added all files

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const path = require("path");
+const uuid = require("uuid");
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+const PORT = 3333;
+
+const reservedTables = [];
+const waitListTable = [];
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+app.get("/tables", function (req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+app.get("/reservations", function (req, res) {
+  res.sendFile(path.join(__dirname, "reservations.html"));
+});
+
+app.post("/api/tables", function (req, res) {
+  let reservedTable = req.body;
+  reservedTable.id = uuid.v4();
+  if (reservedTables.length === 5) {
+    waitListTable.push(reservedTable);
+  } else {
+    reservedTables.push(reservedTable);
+  }
+  res.send(reservedTable);
+});
+app.get("/api/reserved", function (req, res) {
+  res.json(reservedTables);
+});
+app.get("/api/waitList", function (req, res) {
+  res.json(waitListTable);
+});
+
+app.listen(PORT, () => {
+  console.log("Your server started at the port: " + PORT);
+});
