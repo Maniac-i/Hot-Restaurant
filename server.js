cc845d3d1566c67e121863101b96e8bf472c89de
Pavel Darii <paveldarii@yahoo.com>
2021-01-05 20:13:11 -0500

    added some functionality to form.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const waitListTable = [];
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
-app.get("/tables", function (req, res) {
-  res.sendFile(path.join(__dirname, "tables.html"));
+app.get("/form", function (req, res) {
+  res.sendFile(path.join(__dirname, "form.html"));
 });
 app.get("/reservations", function (req, res) {
   res.sendFile(path.join(__dirname, "reservations.html"));
